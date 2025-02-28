a068311033f89a4887ee16367d8b8e3dfae47621
colelevy08 <colelevy08@gmail.com>
2025-02-28 15:37:59 -0500

    publish

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import linkedin from '../../assets/linkedin.svg'
 import githubIcon from '../../assets/github.svg'
 import whatsapp from '../../assets/whatsapp.svg'
 import telegram from '../../assets/telegram.svg'
-{/*import instagramIcon from '../../assets/instagram.svg'*/}
+/*import instagramIcon from '../../assets/instagram.svg'*/
 
 export function Footer() {
   return (
